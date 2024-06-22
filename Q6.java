308f48db92ead2ca7198a376a6805217941ecf27
ShubhCoder-17 <1705patrashubhadeep@gmail.com>
2024-06-22 22:08:16 +0530

    New basic question solved

---
+++ Q6.java
@@ -0,0 +1,7 @@
+public class Q6 {
+    public static void main(String[] args) {
+        String myLetter= "Dear Shubh, \n\tthis course is nice. \n\tThanks!";
+        System.out.println(myLetter);
+    }
+    
+}
