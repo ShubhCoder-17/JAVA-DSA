f19857572a862418d9e3bec8e14d27bd303f6331
ShubhCoder-17 <1705patrashubhadeep@gmail.com>
2024-06-24 09:05:29 +0530

    Typecasting in java

---
+++ Q8.java
@@ -0,0 +1,12 @@
+public class Q8 {
+    public static void main(String[] args) {
+        char grade = 'A';
+        
+        grade = (char) (grade + 8);
+        
+        System.out.println(grade);
+
+        grade = (char) (grade - 8);
+        System.out.println(grade);
+    }
+}
